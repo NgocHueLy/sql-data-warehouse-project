417b9a6d2ffe6f8f40f8502781fb883f5ba47512
Ly Ngoc Hue <45974044+NgocHueLy@users.noreply.github.com>
2025-10-15 21:48:27 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -9,7 +9,10 @@ Script Purpose:
   The database is already created with PgAdmin.
   If the schema already exists, it dropped and recreated. 3 schemas are added for the database: 'bronze', 'silver', and 'gold'.
 
-
+WARNING:
+  Running this script will drop all the schemas if they are exists.
+  All data in the schemas will be permanently deleted. Proceed with caution
+  and ensure you have proper backups before running this script.
 
 */
 
